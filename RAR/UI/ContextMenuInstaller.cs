179cfae72124860ba5dcc4f3b2da5642fdb7235f
TimaDawaa <tima302t@gmail.com>
2025-06-21 14:02:57 +0300

    Integration with the right menu in Windows

---
+++ RAR/UI/ContextMenuInstaller.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RAR.UI
+{
+    class ContextMenuInstaller
+    {
+        private const string AppName = "FileCompressor";
+        private const string MenuText = "Compress with File Compressor";
+        private const string MenuCommand = "compress";
+
+        public static void InstallContextMenu()
+        {
+            try
+            {
+                // Register for all files
+                using (RegistryKey key = Registry.ClassesRoot.CreateSubKey("*\\shell\\" + AppName))
+                {
+                    key.SetValue("", MenuText);
+                    key.SetValue("Icon", Application.ExecutablePath);
+
+                    using (RegistryKey commandKey = key.CreateSubKey("command"))
+                    {
+                        string exePath = $"\"{Application.ExecutablePath}\" \"{MenuCommand}\" \"%1\"";
+                        commandKey.SetValue("", exePath);
+                    }
+                }
+
+                // Register for folders
+                using (RegistryKey key = Registry.ClassesRoot.CreateSubKey("Directory\\shell\\" + AppName))
+                {
+                    key.SetValue("", MenuText);
+                    key.SetValue("Icon", Application.ExecutablePath);
+
+                    using (RegistryKey commandKey = key.CreateSubKey("command"))
+                    {
+                        string exePath = $"\"{Application.ExecutablePath}\" \"{MenuCommand}\" \"%1\"";
+                        commandKey.SetValue("", exePath);
+                    }
+                }
+
+                MessageBox.Show("Context menu installed successfully!", "Success",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Please run as administrator to install context menu.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error installing context menu: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void UninstallContextMenu()
+        {
+            try
+            {
+                Registry.ClassesRoot.DeleteSubKeyTree("*\\shell\\" + AppName, false);
+                Registry.ClassesRoot.DeleteSubKeyTree("Directory\\shell\\" + AppName, false);
+
+                MessageBox.Show("Context menu uninstalled successfully!", "Success",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error uninstalling context menu: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ RAR/UI/ContextMenuManagerForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RAR.UI
+{
+    class ContextMenuManagerForm : Form
+    {
+        private Button installBtn;
+        private Button uninstallBtn;
+        private Label statusLabel;
+
+        public ContextMenuManagerForm()
+        {
+            InitializeComponents();
+        }
+
+        private void InitializeComponents()
+        {
+            this.Text = "Context Menu Manager";
+            this.Size = new System.Drawing.Size(400, 200);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.MaximizeBox = false;
+
+            installBtn = new Button()
+            {
+                Text = "Install Context Menu",
+                Size = new System.Drawing.Size(180, 40),
+                Location = new System.Drawing.Point(30, 50)
+            };
+            installBtn.Click += (s, e) => ContextMenuInstaller.InstallContextMenu();
+
+            uninstallBtn = new Button()
+            {
+                Text = "Uninstall Context Menu",
+                Size = new System.Drawing.Size(180, 40),
+                Location = new System.Drawing.Point(30, 100)
+            };
+            uninstallBtn.Click += (s, e) => ContextMenuInstaller.UninstallContextMenu();
+
+            statusLabel = new Label()
+            {
+                Text = "Manage Windows context menu integration",
+                Location = new System.Drawing.Point(30, 20),
+                AutoSize = true
+            };
+
+            this.Controls.Add(installBtn);
+            this.Controls.Add(uninstallBtn);
+            this.Controls.Add(statusLabel);
+        }
+    }
+}
